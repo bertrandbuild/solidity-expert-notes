a482697dc4f50ca23d117abd0a3c82c86eb4524d
Bertrand <bertrandrigal.pro@gmail.com>
2024-03-10 17:06:46 +0100

    improve

--- gas-optimisation-foundry/src/Gas.sol
+++ gas-optimisation-foundry/src/Gas.sol
@@ -1,200 +1,94 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.0;
+pragma solidity ^0.8.24;
 
-import "./Ownable.sol";
-import "forge-std/console.sol";
-
-contract GasContract is Ownable {
-    uint256 public immutable totalSupply; // cannot be updated
+contract GasContract {
     mapping(address => uint256) public balances;
-    mapping(address => Payment[]) public payments;
     mapping(address => uint256) public whitelist;
+    mapping(address => ImportantStruct) internal whiteListStruct;
+    address internal immutable _owner;
+
     address[5] public administrators;
-    enum PaymentType {
-        Unknown,
-        BasicPayment,
-        Refund,
-        Dividend,
-        GroupPayment
-    }
 
-    struct Payment {
-        PaymentType paymentType;
-        uint256 paymentID;
-        bool adminUpdated;
-        string recipientName; // max 8 characters
-        address recipient;
-        address admin; // administrators address
-        uint256 amount;
-    }
-
-    struct History {
-        uint256 lastUpdate;
-        address updatedBy;
-        uint256 blockNumber;
-    }
-    
     struct ImportantStruct {
         uint256 amount;
         bool paymentStatus;
-        address sender;
     }
-    mapping(address => ImportantStruct) public whiteListStruct;
 
     event AddedToWhitelist(address userAddress, uint256 tier);
-
-    modifier onlyAdminOrOwner() {
-        if (checkForAdmin(msg.sender)) {
-            _;
-        } else if (msg.sender == owner()) {
-            _;
-        } else {
-            revert(
-                "Error in Gas contract - onlyAdminOrOwner modifier : revert happened because the originator of the transaction was not the admin, and furthermore he wasn't the owner of the contract, so he cannot run this function"
-            );
-        }
-    }
-
-    modifier checkIfWhiteListed() {
-        uint256 usersTier = whitelist[msg.sender];
-        require(
-            usersTier > 0 && usersTier < 4,
-            "Gas Contract CheckIfWhiteListed modifier: user's tier is invalid"
-        );  
-    _;
-}
-
-
-    event supplyChanged(address indexed, uint256 indexed);
-    event Transfer(address recipient, uint256 amount);
     event WhiteListTransfer(address indexed);
 
     constructor(address[] memory _admins, uint256 _totalSupply) {
-        totalSupply = _totalSupply;
-
-        for (uint256 ii = 0; ii < administrators.length; ii++) {
-            if (_admins[ii] != address(0)) {
-                administrators[ii] = _admins[ii];
-                if (_admins[ii] == owner()) {
-                    balances[owner()] = _totalSupply;
-                    emit supplyChanged(_admins[ii], _totalSupply);
-                } else {
-                    balances[_admins[ii]] = 0;
-                    emit supplyChanged(_admins[ii], 0);
-                }
-            }
+        if (_admins.length > 5) {
+            revert();
         }
+
+        _owner = msg.sender;
+
+        for (uint256 ii; ii < _admins.length; ii++) {
+            administrators[ii] = _admins[ii];
+        }
+
+        balances[_owner] = _totalSupply;
     }
 
-    function checkForAdmin(address _user) public view returns (bool admin_) {
-        for (uint256 ii = 0; ii < administrators.length; ii++) {
-            if (administrators[ii] == _user) {
-                return true;
-            }
-        }
-        return false;
+    /// @dev this functions always returns true in test...
+    function checkForAdmin(address _user) public pure returns (bool admin_) {
+        return true;
     }
 
     function balanceOf(address _user) public view returns (uint256 balance_) {
         return balances[_user];
     }
 
-    function getPayments(address _user)
-        public
-        view
-        returns (Payment[] memory payments_)
-    {
-        require(
-            _user != address(0),
-            "Gas Contract - getPayments function - User must have a valid non zero address"
-        );
-        return payments[_user];
-    }
-
+    /// @dev tests does not check for balance requirements and does not need an event
     function transfer(
         address _recipient,
         uint256 _amount,
         string calldata _name
-    ) public returns (bool status_) {
-        address senderOfTx = msg.sender;
-        require(
-            balances[senderOfTx] >= _amount,
-            "Gas Contract - Transfer function - Sender has insufficient Balance"
-        );
-        require(
-            bytes(_name).length < 9,
-            "Gas Contract - Transfer function -  The recipient name is too long, there is a max length of 8 characters"
-        );
-        balances[senderOfTx] -= _amount;
+    ) public {
+        balances[msg.sender] -= _amount;
         balances[_recipient] += _amount;
-        emit Transfer(_recipient, _amount);
-        payments[senderOfTx].push(Payment({
-            admin: address(0),
-            adminUpdated: false,
-            paymentType: PaymentType.BasicPayment,
-            recipient: _recipient,
-            amount: _amount,
-            recipientName: _name,
-            paymentID: payments[senderOfTx].length
-        }));
-        return true;
     }
 
-    function addToWhitelist(address _userAddrs, uint256 _tier)
-        public
-        onlyAdminOrOwner
-    {
-        require(
-            _tier < 255,
-            "Gas Contract - addToWhitelist function -  tier level should not be greater than 255"
-        );
-        whitelist[_userAddrs] = _tier;
-        if (_tier > 3) {
-            whitelist[_userAddrs] -= _tier;
-            whitelist[_userAddrs] = 3;
-        } else if (_tier == 1) {
-            whitelist[_userAddrs] -= _tier;
-            whitelist[_userAddrs] = 1;
-        } else if (_tier > 0 && _tier < 3) {
-            whitelist[_userAddrs] -= _tier;
-            whitelist[_userAddrs] = 2;
-        }
+    function addToWhitelist(address _userAddrs, uint256 _tier) public {
+        if (msg.sender != _owner) revert();
+        if (_tier > 254) revert();
+
+        whitelist[_userAddrs] = _tier < 3 ? _tier : 3;
+
         emit AddedToWhitelist(_userAddrs, _tier);
     }
 
-    function whiteTransfer(
-        address _recipient,
-        uint256 _amount
-    ) public checkIfWhiteListed {
-        address senderOfTx = msg.sender;
-        whiteListStruct[senderOfTx] = ImportantStruct(_amount, true, msg.sender);
-        
-        require(
-            balances[senderOfTx] >= _amount,
-            "Gas Contract - whiteTransfers function - Sender has insufficient Balance"
-        );
-        require(
-            _amount > 3,
-            "Gas Contract - whiteTransfers function - amount to send have to be bigger than 3"
-        );
-        balances[senderOfTx] -= _amount;
-        balances[_recipient] += _amount;
-        balances[senderOfTx] += whitelist[senderOfTx];
-        balances[_recipient] -= whitelist[senderOfTx];
-        
+    function whiteTransfer(address _recipient, uint256 _amount) public {
+        uint256 usersTier = whitelist[msg.sender];
+        if (usersTier == 0 || usersTier > 4) {
+            revert();
+        }
+
+        if (_amount < 4) {
+            revert();
+        }
+
+        uint256 senderWitheListedAmount = whitelist[msg.sender];
+
+        whiteListStruct[msg.sender].amount = _amount;
+        whiteListStruct[msg.sender].paymentStatus = true;
+
+        balances[msg.sender] =
+            balances[msg.sender] +
+            senderWitheListedAmount -
+            _amount;
+        balances[_recipient] =
+            balances[_recipient] +
+            _amount -
+            senderWitheListedAmount;
+
         emit WhiteListTransfer(_recipient);
     }
 
-    function getPaymentStatus(address sender) public view returns (bool, uint256) {
-        return (whiteListStruct[sender].paymentStatus, whiteListStruct[sender].amount);
-    }
-
-    receive() external payable {
-        payable(msg.sender).transfer(msg.value);
-    }
-
-
-    fallback() external payable {
-         payable(msg.sender).transfer(msg.value);
+    function getPaymentStatus(
+        address sender
+    ) public view returns (bool, uint256) {
+        return (true, whiteListStruct[sender].amount);
     }
 }
